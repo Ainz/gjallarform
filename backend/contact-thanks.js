cf36dbb29f5ff1d4df581dcd01c643cd63cc268d
SecCon <3930716+Ainz@users.noreply.github.com>
2025-10-08 14:31:10 +0200

    Delete backend/contact-thanks.js

--- backend/contact-thanks.js
+++
@@ -1,6 +0,0 @@
-/*!
- * DEPRECATED: This file has moved.
- * Use https://service.conram.it/contactulus/contactulus.js instead.
- * This stub will be removed in the next release.
- */
-console.warn('[Contactulus] This file is deprecated. Use the hosted script instead.');
